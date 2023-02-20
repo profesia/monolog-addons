de98ee588de08c9490d4168e7c87473e3a00b35c
Matej Bádal <badal@profesia.sk>
2023-02-20 15:43:36 +0100

    Added deprecation tag to internal correlation ID resolver

--- src/Extra/CorrelationIdGeneratorInterface.php
+++ src/Extra/CorrelationIdGeneratorInterface.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Profesia\Monolog\Extra;
 
+/**
+ * @deprecated
+ */
 interface CorrelationIdGeneratorInterface
 {
     public function generate(): string;
--- src/Extra/Uuid4Generator.php
+++ src/Extra/Uuid4Generator.php
@@ -6,6 +6,7 @@ namespace Profesia\Monolog\Extra;
 
 use Ramsey\Uuid\Uuid;
 
+
 final class Uuid4Generator implements CorrelationIdGeneratorInterface
 {
     public function generate(): string
