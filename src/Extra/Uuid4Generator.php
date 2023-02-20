904ed298b511f0d3f310db397ed4e41783f907ad
Matej Bádal <badal@profesia.sk>
2023-02-20 15:53:59 +0100

    Added deprecaton flag also to Uuid4 implementation

--- src/Extra/Uuid4Generator.php
+++ src/Extra/Uuid4Generator.php
@@ -6,7 +6,9 @@ namespace Profesia\Monolog\Extra;
 
 use Ramsey\Uuid\Uuid;
 
-
+/**
+ * @deprecated 
+ */
 final class Uuid4Generator implements CorrelationIdGeneratorInterface
 {
     public function generate(): string
